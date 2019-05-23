d4da64a7e8e9bccb913db4c6f67e23b60b0dbd2a
Shaunak <shaunak241997@gmail.com>
2019-05-23 16:57:46 +0530

    Record 1.3 Django 1.2

--- app/src/main/java/android/example/com/prescriptminder/ProfileActivity.java
+++ app/src/main/java/android/example/com/prescriptminder/ProfileActivity.java
@@ -1,54 +1,78 @@
 package android.example.com.prescriptminder;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProfileActivity extends AppCompatActivity {
 
-    private TextView responseText;
-    private RequestQueue requestQueue;
     private Button sendButton;
+    private TextView responseText;
     private EditText urlEditText;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        responseText = findViewById(R.id.response);
         sendButton = findViewById(R.id.url_send_button);
+        responseText = findViewById(R.id.response);
         urlEditText = findViewById(R.id.url);
+        imageView = findViewById(R.id.imageView);
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, urlEditText.getText().toString(), new Response.Listener<String>() {
+                StringRequest postRequest = new StringRequest(Request.Method.POST, urlEditText.getText().toString().trim(),
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response) {
+                                // response
+                                responseText.setText(response);
+                                Log.d("Response", response);
+                                showToast("Hua");
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // error
+                                responseText.setText(error.toString());
+                                Log.d("Error.Response", error.toString());
+                                showToast("Nahi hua");
+                            }
+                        }
+                ) {
                     @Override
-                    public void onResponse(String response) {
-                        responseText.setText(response);
-                        showToast("Response received");
+                    protected Map<String, String> getParams() {
+                        Map<String, String>  params = new HashMap<>();
+                        params.put("username", "shaunak24");
+                        params.put("password", "nusta123");
+                        return params;
                     }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        responseText.setText(error.toString());
-                        showToast("Kuch to gadbad hai daya");
-                    }
-                });
-                MySingleton.getInstance(getApplicationContext()).addRequestToQueue(stringRequest);
+                };
+                MySingleton.getInstance(getApplicationContext()).addRequestToQueue(postRequest);
+                Bitmap bmp = QRCodeUtil.encodeAsBitmap("Shaunak", 800, 800);
+                imageView.setImageBitmap(bmp);
             }
-
         });
 
     }
